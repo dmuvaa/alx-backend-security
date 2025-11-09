45fca34425fdb5412ea2de4e479353ac36c823a8
Dennis Muvaa <dmuvaa70@gmail.com>
2025-11-09 23:44:31 +0300

    Update IP Blocking

--- ip_tracking/middleware.py
+++ ip_tracking/middleware.py
@@ -1,12 +1,13 @@
+from django.http import HttpResponseForbidden
 from django.utils import timezone
 
-from .models import RequestLog
+from .models import RequestLog, BlockedIP
 
 
 class IPTrackingMiddleware:
     """
     Middleware that logs the IP address, timestamp, and path
-    of every incoming request.
+    of every incoming request and blocks blacklisted IPs.
     """
 
     def __init__(self, get_response):
@@ -15,6 +16,11 @@ class IPTrackingMiddleware:
     def __call__(self, request):
         ip_address = self.get_client_ip(request)
 
+        # Check if IP is blocked
+        if BlockedIP.objects.filter(ip_address=ip_address).exists():
+            return HttpResponseForbidden("Forbidden: Your IP address is blocked.")
+
+        # Log request
         RequestLog.objects.create(
             ip_address=ip_address,
             timestamp=timezone.now(),
